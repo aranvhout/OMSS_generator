789da7eceb73e986816989109b9ee8c6959e0c51
aranvhout <aranvanhout@hotmail.com>
2025-05-22 12:19:15 +0200

    updated main to work for our awesome package

--- main.py
+++ main.py
@@ -1,44 +1,15 @@
-# main.py
-# a minimal example of the generator
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 19 15:37:42 2025
 
-# files needed: matrix.py, render.py, alternatives.py, rules.py, configuration.py, entity.py, seed.py
-
-from rules import Ruletype, AttributeType, Rule
-from matrix import create_matrix
-
-#import matplotlib.pyplot as plt
-from PIL import Image
-
-
-from PIL import Image
-import numpy as np
-
-# Simple example, everything constant but progressing angle
-r1 = {
-    'BigShape': [
-        Rule(Ruletype.FULL_CONSTANT, AttributeType.SHAPE, value = 'square'),
-        Rule(Ruletype.PROGRESSION, AttributeType.ANGLE),
-        Rule(Ruletype.FULL_CONSTANT, AttributeType.COLOR, value = 'blue'),
-        Rule(Ruletype.FULL_CONSTANT, AttributeType.NUMBER),
-        Rule(Ruletype.FULL_CONSTANT, AttributeType.SIZE)]}
+@author: Work
+"""
+#in order to see changes in src reflected in main directly, run the following command in the directory that contaisn the src folder and the pyproject.toml
+#pip install -e .
+import omss
+from omss import Ruletype, AttributeType, Rule, create_matrix
 
 
-#solution_matrix, problem_matrix, alternatives = create_matrix(r1, alternatives=4, seed = None,  alternative_seed =None ,save = False, entity_types=[ 'BigShape',])
-
-print(alternatives)
-# solution_matrix is a bit map
-
-
-
-#array = np.array(solution_matrix, dtype=np.uint8)
-#image = Image.fromarray(array, mode='RGB')
-#image.save("soluxxx_matrix.png")
-
-
-
-
-
-# Define rules for each entity
 rules = {
     'BigShape': [
         Rule(Ruletype.CONSTANT, AttributeType.SHAPE),
@@ -67,7 +38,4 @@ rules = {
 #image.save("color_bitmap_output.png")
 
 
-create_matrix(rules, alternatives=4, seed = 5,  alternative_seed =None ,save =True, entity_types=[ 'BigShape', 'LittleShape' ])
-
-#12345788912457
-#111245511123,
+create_matrix(rules, alternatives=4, seed = 13,  alternative_seed =None ,save =True, entity_types=[ 'BigShape', 'LittleShape' ])
